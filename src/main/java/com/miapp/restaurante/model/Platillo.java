4f8721e74faa250ae789ba5eeaa31b118ffb39db
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-16 20:27:39 -0300

    feat: crear modelo Platillo

--- src/main/java/com/miapp/restaurante/model/Platillo.java
+++ src/main/java/com/miapp/restaurante/model/Platillo.java
@@ -1,4 +1,25 @@
 package com.miapp.restaurante.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Platillo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_platillo")
+    private Long idPlatillo;
+    @Column(name = "nom_platillo", nullable = false)
+    private String nombrePlatillo;
+    @Column(name = "descripcion", nullable = true)
+    private String descripcion;
+    @Column(name = "precio", nullable = false)
+    private Double precio;
 }
