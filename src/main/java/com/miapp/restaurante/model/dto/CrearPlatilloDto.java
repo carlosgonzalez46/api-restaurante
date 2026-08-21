7867566c236ef7ed15693b726f7c6ac67402aab6
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-21 10:53:53 -0300

    feat: crear dto CrearPlatilloDto

---
+++ src/main/java/com/miapp/restaurante/model/dto/CrearPlatilloDto.java
@@ -0,0 +1,24 @@
+package com.miapp.restaurante.model.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CrearPlatilloDto {
+    @NotBlank(message = "Campo nombre de platillo es obligatorio")
+    @Size(max = 100, message = "Maximo de 100 caracteres permitidos")
+    private String nombrePlatillo;
+    @Size(max = 255, message = "Maximo de 255 caracteres permitidos")
+    private String descripcion;
+    @Positive(message = "El precio debe ser un numero positivo")
+    @NotNull(message = "Campo precio de platillo es obligatorio")
+    private Long precio;
+}
