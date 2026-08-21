4192d40fafd0ed87b422af510378498590808779
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-21 17:56:16 -0300

    feat: crear dto ActualizarClienteDto

---
+++ src/main/java/com/miapp/restaurante/model/dto/ActualizarClienteDto.java
@@ -0,0 +1,22 @@
+package com.miapp.restaurante.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActualizarClienteDto {
+    @NotBlank(message = "Campo nombre del cliente es obligatorio")
+    @Size(max = 70, message = "Maximo de 70 caracteres permitido")
+    private String nombreCliente;
+    @Email
+    private String correoElectronico;
+    @NotBlank(message = "Campo direccion del cliente es obligatorio")
+    @Size(max = 100, message = "Maximo de 100 caracteres permitido")
+    private String direccion;
+}
