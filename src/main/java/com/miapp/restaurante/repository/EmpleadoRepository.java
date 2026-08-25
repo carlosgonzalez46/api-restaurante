c91c45dab6b5104b3d0877e2165c837503c1749c
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-25 14:53:06 -0300

    feat: crear EmpleadoRepository

---
+++ src/main/java/com/miapp/restaurante/repository/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package com.miapp.restaurante.repository;
+
+import com.miapp.restaurante.model.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+}
