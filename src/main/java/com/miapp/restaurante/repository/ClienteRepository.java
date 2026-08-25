7123098970893aecdefb8ef45397284da38e8f71
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-25 14:50:38 -0300

    feat: crear ClienteRepository

---
+++ src/main/java/com/miapp/restaurante/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.miapp.restaurante.repository;
+
+import com.miapp.restaurante.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
