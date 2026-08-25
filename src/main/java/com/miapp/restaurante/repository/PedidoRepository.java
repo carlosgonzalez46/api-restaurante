38f5f2ae11b0259af11a299546905baefdb64d36
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-25 15:02:25 -0300

    feat: crear PedidoRepository

---
+++ src/main/java/com/miapp/restaurante/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.miapp.restaurante.repository;
+
+import com.miapp.restaurante.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
