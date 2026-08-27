e2b88b8b7a2fb6b03a988af4db827a59bda00ccb
Carlos Gonzalez <elgz8623@gmail.com>
2026-08-27 09:50:23 -0300

    fix: renombrar dto ActualizarDetallePedidoDto

---
+++ src/main/java/com/miapp/restaurante/model/dto/ActualizarDetallePedidoDto.java
@@ -0,0 +1,25 @@
+package com.miapp.restaurante.model.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActualizarDetallePedidoDto {
+    @Positive(message = "La cantidad debe ser mayor a cero")
+    @NotNull(message = "El campo cantidad es obligatorio")
+    private int cantidad;
+    @Positive(message = "El precio unitario debe ser mayor a cero")
+    @NotNull(message = "El campo precio unitario es obligatorio")
+    private Long precioUnitario;
+    @Positive
+    @NotNull(message = "El campo id de pedido es obligatorio")
+    private Long idPedido;
+    @Positive
+    @NotNull(message = "El campo id de platillo es obligatorio")
+    private Long idPlatillo;
+}
